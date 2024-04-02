ce3a50050a3cd21a203831bc79b49ceed6e88250
Novin1380 <125552167+Novin1380@users.noreply.github.com>
2024-04-02 22:56:30 +0330

    some changes and BaseAuthentication for admin

---
+++ core/accounts/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User,Profile
+# Register your models here.
+
+
+        
+        
+class CustomUserAdmin(UserAdmin):
+    # add_form = CustomUserCreationForm 
+    model = User
+    list_display = ('email','is_staff','is_active')
+    list_filter = ('email','is_staff','is_active')
+    searching_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", 'is_superuser')}),
+        ("group permissions", {"fields": ("groups","user_permissions")}),
+        ("important date ", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1","password2", "is_staff",
+                "is_active", "is_superuser"
+            )}
+        ),
+    )
+    
+admin.site.register(Profile)
+admin.site.register(User , CustomUserAdmin)
