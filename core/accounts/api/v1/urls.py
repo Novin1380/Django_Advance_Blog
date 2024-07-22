517c306911e9117a6a76804df8605d1dd45a6915
Novin1380 <125552167+Novin1380@users.noreply.github.com>
2024-07-22 11:56:18 +0330

    Custom auth serializer

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,15 +1,22 @@
 from django.urls import path,include
 from django.views.generic import TemplateView,RedirectView
-from .views import RegistrationApiView
+from .views import RegistrationApiView,CustomObtainAuthToken,CustomDiscardAuthToken
+# from rest_framework.authtoken.views import ObtainAuthToken
 
 
 app_name= 'api-v1'
 
 urlpatterns = [
     #resgistration
-    path('registration/',RegistrationApiView.as_view(), name='registration')
+    path('registration/',RegistrationApiView.as_view(), name='registration'),
+    #login token
+    path('token/login/',CustomObtainAuthToken.as_view(), name='token-login'),
+    # path("token/login",ObtainAuthToken.as_view() , name = 'token-login')
+    #logout token
+    path('token/logout/',CustomDiscardAuthToken.as_view(), name='token-logout'),
     #change password
     #reset password
+    #
     
 
 
